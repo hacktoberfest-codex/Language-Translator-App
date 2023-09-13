adf67710664f572f218e0e7c77e34a4d65617cfa
Aryaman Patra <aryamanprivate26@gmail.com>
2023-09-13 18:54:30 +0530

    Adding features fragment_text.xml

--- app/src/main/java/com/example/translatorapp/ui/elements/MainActivityFragments/TextFragment.java
+++ app/src/main/java/com/example/translatorapp/ui/elements/MainActivityFragments/TextFragment.java
@@ -2,6 +2,8 @@ package com.example.translatorapp.ui.elements.MainActivityFragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -62,5 +64,16 @@ public class TextFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_text, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    public void switchTextBox(View view){
+        
     }
 }
