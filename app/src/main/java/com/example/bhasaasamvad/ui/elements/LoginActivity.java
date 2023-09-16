43fbe589444d550aa1a0eabff610b11a40c1c26a
Aryaman Patra <aryamanprivate26@gmail.com>
2023-09-16 22:54:37 +0530

    Added login,signup,recoverypage and lots of drawable files; also close app on double tap

--- app/src/main/java/com/example/bhasaasamvad/ui/elements/LoginActivity.java
+++ app/src/main/java/com/example/bhasaasamvad/ui/elements/LoginActivity.java
@@ -1,14 +1,17 @@
-package com.example.bhasaasamvad;
+package com.example.bhasaasamvad.ui.elements;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.bhasaasamvad.R;
+
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/example/bhasaasamvad/ui/elements/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.bhasaasamvad.ui.elements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.example.bhasaasamvad.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean doubleBackToExitPressedOnce = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            finishAffinity();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
+}
